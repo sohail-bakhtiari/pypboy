b6273c3353138f96ba843b8b0ad8d3968f768bcb
Sohail Bakhtiari <sbakhtiari@gmail.com>
2026-01-25 02:05:26 +0100

    Show IP address in DATA module

--- pypboy/modules/data/misc.py
+++ pypboy/modules/data/misc.py
@@ -1,5 +1,6 @@
 import pypboy
 import settings
+import socket
 
 class Module(pypboy.SubModule):
 
@@ -15,7 +16,19 @@ class Module(pypboy.SubModule):
 		self.topmenu.label = "DATA"
 		self.topmenu.title = settings.MODULE_TEXT
 
-		settings.FOOTER_TIME[2] = ""
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			try:
+				s.connect(('8.8.8.8', 80))
+				ip = s.getsockname()[0]
+			except Exception:
+				ip = '127.0.0.1'
+			finally:
+				s.close()
+		except Exception:
+			ip = '127.0.0.1'
+   
+		settings.FOOTER_TIME[2] = ip
 		self.footer = pypboy.ui.Footer(settings.FOOTER_TIME)
 		self.footer.rect[0] = settings.footer_x
 		self.footer.rect[1] = settings.footer_y
